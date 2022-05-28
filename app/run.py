9a7d20052531bc8f0c1bfd79e2b0b96f9558a800
cmgboris5 <103899117+cmgboris5@users.noreply.github.com>
2022-05-28 11:14:53 -0400

    Add files via upload

--- app/run.py
+++ app/run.py
@@ -1,19 +1,40 @@
 import json
 import plotly
 import pandas as pd
-import numpy as np
 
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk import pos_tag, word_tokenize
+import nltk
+
+from sklearn.base import BaseEstimator, TransformerMixin
 
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -27,8 +48,8 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('FigureEight', engine)
+engine = create_engine('sqlite:///../data/disaster_response_db.db')
+df = pd.read_sql_table('df', engine)
 
 # load model
 model = joblib.load("../models/classifier.pkl")
@@ -38,25 +59,20 @@ model = joblib.load("../models/classifier.pkl")
 @app.route('/')
 @app.route('/index')
 def index():
-
+    
     # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-
-    # Show distribution of different category
-    category = list(df.columns[4:])
-    category_counts = []
-    for column_name in category:
-        category_counts.append(np.sum(df[column_name]))
-
-    # extract data exclude related
-    categories = df.iloc[:,4:]
-    categories_mean = categories.mean().sort_values(ascending=False)[1:11]
-    categories_names = list(categories_mean.index)
-
-
+    
+    category_names = df.iloc[:,4:].columns
+    category_boolean = (df.iloc[:,4:] != 0).sum().values
+    
+    
     # create visuals
+    # TODO: Below is an example - modify to create your own visuals
     graphs = [
+            # GRAPH 1 - genre graph
         {
             'data': [
                 Bar(
@@ -75,11 +91,12 @@ def index():
                 }
             }
         },
+            # GRAPH 2 - category graph    
         {
             'data': [
                 Bar(
-                    x=category,
-                    y=category_counts
+                    x=category_names,
+                    y=category_boolean
                 )
             ],
 
@@ -89,48 +106,32 @@ def index():
                     'title': "Count"
                 },
                 'xaxis': {
-                    'title': "Category"
-                }
-            }
-        },
-        {
-            'data': [
-                Bar(
-                    x=categories_names,
-                    y=categories_mean
-                )
-            ],
-
-            'layout': {
-                'title': 'Top 10 Message Categories',
-                'yaxis': {
-                    'title': "Percentage"
-                },
-                'xaxis': {
-                    'title': "Categories"
+                    'title': "Category",
+                    'tickangle': 35
                 }
             }
         }
     ]
-
+    
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-
+    
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '')
+    query = request.args.get('query', '') 
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file.
+    # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
         query=query,
